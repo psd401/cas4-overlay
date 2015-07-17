9465794056bff320ede393fdc2aaacd4fe68fa21
Brandon Martin <martinb@psd401.net>
2015-07-17 18:15:21 +0000

    Fixing typos

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git pull origin master
 \cp -f /tmp/cas4-overlay/etc/*.* /etc/cas
 mvn clean package
 rm -Rf /var/lib/tomcat/webapps/ROOT
-\cp -f /tmp/cas4-overlay/target/cas.war /var/lib/tomcat/webapps/ROOT.war
-systemctl restart tomcat
+\cp -f /tmp/cas4-overlay/target/cas.war /var/lib/tomcat7/webapps/ROOT.war
+service tomcat7 restart
